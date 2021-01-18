ea3bf023369f59a1cc72e9b482fe20b26137d7a6
hzy46 <362583303@qq.com>
2021-01-18 14:17:47 +0800

    fix

--- contrib/kubespray/script/kubernetes-boot.sh
+++ contrib/kubespray/script/kubernetes-boot.sh
@@ -2,9 +2,9 @@
 
 echo "setup k8s cluster"
 cd ${HOME}/pai-deploy/kubespray
-# Ubuntu 20.04 doesn't have python 2 pre-installed.
-# So use python3 as the interpreter.
-# Ubuntu 16.04, 18.04, and 20.04 all have it pre-installed.
+# Ubuntu 20.04 doesn't have python2 pre-installed,
+# but Ubuntu 16.04, 18.04, and 20.04 all have python3 pre-installed.
+# So we use python3 as the default interpreter.
 ansible-playbook -i inventory/pai/hosts.yml -e "ansible_python_interpreter=/usr/bin/python3" cluster.yml --become --become-user=root -e "@inventory/pai/openpai.yml" || exit $?
 
 sudo mkdir -p ${HOME}/pai-deploy/kube || exit $?
